49e9d64665ef959b14f22d82d1259aff0914c67e
Ivan <sgoidxxl@gmail.com>
2017-06-13 06:25:29 +0300

    Initial commit

---
+++ CityTransport/src/services/database/entities/Driver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+
+namespace CityTransport.src.services.database.entities
+{
+    class Driver
+    {
+        public int Id;
+        public String Name;
+    }
+    
+    class DriverContext : DbContext
+    {
+        public DriverContext() : base("DatabaseConnection")
+        {
+
+        }
+
+        public DbSet<Driver> Drivers;
+    }
+}
---
+++ CityTransport/src/views/Form1.cs
@@ -0,0 +1,36 @@
+﻿using CityTransport.src.services.database.entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CityTransport
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            using (DriverContext db = new DriverContext())
+            {
+                Driver d1 = new Driver { Name = "Alex" };
+                Driver d2 = new Driver { Name = "Jake" };
+
+                db.Drivers.Add(d1);
+                db.Drivers.Add(d2);
+                db.SaveChanges();
+
+                var drivers = db.Drivers;
+
+                foreach (var driver in drivers)
+                {
+                    Console.WriteLine("Driver {0} with Name {1}", driver.Id, driver.Name);
+                }
+            }
+        }
+    }
+}
